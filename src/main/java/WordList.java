e60d48995a6e298b0d8be9e0ef6dfdc7c41091ed
chelsvaleria <143123776+chelsvaleria@users.noreply.github.com>
2024-04-15 18:04:28 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,23 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int numWords = 0; 
+    for(int i = 0; i < myList.size(); i++){
+    String store = myList.get(i);
+      if(store.size() == len)
+       numWords++;
+    }
+    return numWords;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+for(int i = 0; i < myList.size(); i++){
+    String store = myList.get(i);
+      if(store.size() == len)
+       myList.remove(i);
+    }
+    return myList;
   }
 }
